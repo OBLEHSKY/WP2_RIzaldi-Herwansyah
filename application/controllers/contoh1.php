a6d55844c5b7caf704ada6d4107acc9f99151b10
OBLEHSKY <rzaldih@gmail.com>
2022-03-22 09:26:03 +0700

    Menambakan file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya OBLEHSKY
+        Saya tinggal di daerah NewYork
+        Hobi saya adalah otomotif";
+    }
+}
+?>
